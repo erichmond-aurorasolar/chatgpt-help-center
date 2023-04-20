f302a9d8ab062749d850d9386d1e2ed0627a0a70
Adriel Luo <haoala@gmail.com>
2023-04-20 09:52:41 -0700

    Add demo Q&A website

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+
+from flask import Flask, redirect, render_template, request, url_for
+import numpy as np
+import openai
+from openai.embeddings_utils import distances_from_embeddings
+import pandas as pd
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        question = request.form["question"]
+        answer = get_answer(question)
+        return redirect(url_for("index", result=answer))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+df = pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+
+
+def get_answer(question):
+    answer = answer_question(
+        df, question=question, debug=False, max_len=1800, max_tokens=300,
+    )
+    return answer
+
+
+def answer_question(
+    df,
+    question="Am I allowed to publish model outputs to Twitter, without a human review?",
+    max_len=1800,
+    debug=False,
+    max_tokens=150,
+    stop_sequence=None
+):
+    """
+    Answer a question based on the most similar context from the dataframe texts
+    """
+    context = create_context(
+        question,
+        df,
+        max_len=max_len,
+    )
+    # If debug, print the raw model response
+    if debug:
+        print("Context:\n" + context)
+        print("\n\n")
+
+    try:
+        # Create a completions using the question and context
+        prompt = (f"Answer the question based on the context below, and if the question can't be "
+                  f"answered based on the context, say \"I don't know\"\n\nContext: {context}\n\n"
+                  f"---\n\nQuestion: {question}\nAnswer:")
+
+        response = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {'role': 'user', 'content': prompt},
+            ],
+            temperature=0,
+            stop=stop_sequence,
+            max_tokens=max_tokens,
+        )
+        print(f'{response=}')
+        total_tokens = response['usage']['total_tokens']
+        print(f"{total_tokens=}, estimated_cost=${total_tokens/1000*0.002:.3f}")
+        return question + "\n\n" + response["choices"][0]["message"]["content"]
+    except Exception as e:
+        print(e)
+        return ""
+
+
+def create_context(
+    question, df, max_len=1800,  # size='ada',
+):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(
+        input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(
+        q_embeddings, df['embeddings'].values, distance_metric='cosine')
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+    # Return the context
+    return "\n\n###\n\n".join(returns)
